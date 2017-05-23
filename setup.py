47dc43ff1023c0606c9d3499686af3875fbccc36
Alexey Poryadin <alexey.poryadin@gmail.com>
2017-05-24 01:33:50 +0300

    setup.py that help using Squall to build Python extensions

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(**{
+    'name': 'squall',
+    'version': '0.1.dev20170524',
+    'author': 'Alexey Poryadin',
+    'author_email': 'alexey.poryadin@gmail.com',
+    'description': "Includes files for using the Squall template library"
+                   "to build a Python C/C++ extensions.",
+    'headers': ['include/squall/Dispatcher.hxx',
+                'include/squall/NonCopyable.hxx',
+                'include/squall/PlatformLoop.hxx',
+                'include/squall/PlatformWatcher.hxx'],
+    'zip_safe': False
+})
